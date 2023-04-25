7f85afd7e9fa42d04f227efb0fe9b4479881f5f4
Conor Dunne <conordunne@outlook.ie>
2023-04-26 05:41:16 +0900

    Build login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,12 +1,42 @@
-import NavBar from "scenes/navbar"
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+// import Form from "./Form";
 
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 100px");
   return (
-    <div>
-      <NavBar />
-      Login Page
-    </div>
-  )
-}
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+        >
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+      width={isNonMobileScreens ? "50%" : "93%"}
+      p="2rem"
+      m="2rem auto"
+      borderRadius="1.5rem"
+      backgroundColor={theme.palette.background.alt}
+      >
+        <Typography
+        fontWeight="500"
+        variant="h5"
+        sx= {{mb: "1.5rem"}}
+        >
+          Welcome to Sociopedia.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
